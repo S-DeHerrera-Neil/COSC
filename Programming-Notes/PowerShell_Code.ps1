c52a680c6616e03f5613f574117414be7e058cb6
S-DeHerrera-Neil <neildeherrera94@gmail.com>
2024-07-29 13:06:17 -0400

    Update PowerShell_Code.ps1

--- Programming-Notes/PowerShell_Code.ps1
+++ Programming-Notes/PowerShell_Code.ps1
@@ -1,1 +1,32 @@
-#Insert Notes Here#
+# Finding Executables
+get-ChildItem -path C:\Windows -filter *.exe -recurse -name
+
+# Finding the size of ChildItems
+$cols =@{Label="KB"; Expression = { [int] ($_./Length/1KB) }}
+Get-ChildItem _ Format-Table name, $cols
+
+# Finding Running Services
+get-service | where-object {$_.status -eq "running"}
+
+# Finds stopped services
+get-service | where-object {$_.status -eq 'stopped'}
+
+# Write-Host does not send data down the line
+# function 
+function fruit-host {
+Write-Host "Apple"
+Write-Host "Orange"
+Write-Host "Bannana"
+Write-Host "Pear"
+}
+
+function fruit-Output {
+Write-Output "Apple"
+Write-Output "Orange"
+Write-Output "Bannana"
+Write-Output "Pear"
+}
+
+$x = 10
+Get-Variable x
+Get-ChildItem variable:x`

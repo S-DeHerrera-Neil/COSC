564583cec1086f555f1ad8ffca2320934819200c
S-DeHerrera-Neil <neildeherrera94@gmail.com>
2024-09-18 09:53:02 -0400

    Create JS_Interaction.js

---
+++ Security-Notes/JS_Interaction.js
@@ -0,0 +1,19 @@
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+<script src="https://www.w3schools.com/js/myScript1.js"></script>
+
+JS in action
+
+DEMO: Lets take a look at Javademo.html
+
+
+http://10.50.XX.XX/java/Javademo.html
+Developer Console
+
+(Must be done from opstation!)
+15
+JS Interaction<script> function myFunction() { document.getElementById("demo").innerHTML = "Paragraph changed."; } </script> <script src="https://www.w3schools.com/js/myScript1.js"></script>
